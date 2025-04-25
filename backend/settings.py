11e329931d5d388e85816d076a4e0c6fd6ebafb7
minhcuong2102 <108413114+minhcuong2102@users.noreply.github.com>
2025-04-25 13:44:23 +0700

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -28,9 +28,9 @@ CORS_ALLOW_CREDENTIALS = True
 
 # CORS_ALLOWED_ORIGINS = ["http://192.168.110.40:3000"]
 # CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS').split(',')
-CORS_ALLOW_HEADERS = list(default_headers) + [
-    'authorization',
-]
+# CORS_ALLOW_HEADERS = list(default_headers) + [
+#     'authorization',
+# ]
 CORS_ALLOWED_ORIGINS = [
     "http://192.168.110.40:3000",
     "http://localhost:3000",
