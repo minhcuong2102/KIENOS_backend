338481b8a945e1acb2261f6fc9fa123b1ceedefa
minhcuong2102 <108413114+minhcuong2102@users.noreply.github.com>
2025-06-09 11:56:19 +0700

    Update firebase_utils.py

--- notification/firebase_utils.py
+++ notification/firebase_utils.py
@@ -15,14 +15,19 @@ import json
 #     firebase_admin.initialize_app(cred)
 if not firebase_admin._apps:
     # Lấy biến môi trường FIREBASE_KEY và đảm bảo không None
-    firebase_key_str = os.environ.get("FIREBASE_KEY")
-    if firebase_key_str:
-        # Parse key JSON (chuyển các \\n thành \n cho private_key)
-        firebase_key_dict = json.loads(firebase_key_str.replace("\\n", "\n"))
-        cred = credentials.Certificate(firebase_key_dict)
-        firebase_admin.initialize_app(cred)
-    else:
-        raise ValueError("Biến môi trường FIREBASE_KEY chưa được cấu hình.")
+    # firebase_key_str = os.environ.get("FIREBASE_KEY")
+    # if firebase_key_str:
+    #     # Parse key JSON (chuyển các \\n thành \n cho private_key)
+    #     firebase_key_dict = json.loads(firebase_key_str.replace("\\n", "\n"))
+    #     cred = credentials.Certificate(firebase_key_dict)
+    #     firebase_admin.initialize_app(cred)
+    # else:
+    #     raise ValueError("Biến môi trường FIREBASE_KEY chưa được cấu hình.")
+    firebase_b64 = os.environ.get("FIREBASE_KEY")
+    firebase_key_json = base64.b64decode(firebase_b64).decode("utf-8")
+    firebase_key_dict = json.loads(firebase_key_json)
+    cred = credentials.Certificate(firebase_key_dict)
+    firebase_admin.initialize_app(cred)
         
 def send_push_notification(device_token, title, body, data=None):
     message = messaging.Message(
