62942d28c98667b8a61edf3cc8c4d99b5d7ace93
minhcuong2102 <108413114+minhcuong2102@users.noreply.github.com>
2025-06-09 10:56:07 +0700

    Update firebase_utils.py

--- notification/firebase_utils.py
+++ notification/firebase_utils.py
@@ -1,13 +1,17 @@
 import firebase_admin
 from firebase_admin import credentials, messaging
 import os
+import json
 
 # Lấy đường dẫn tuyệt đối tới file serviceAccountKey.json trong cùng folder
-cred_path = os.path.join(os.path.dirname(__file__), 'serviceAccountKey.json')
-
-# Chỉ khởi tạo Firebase một lần
+# cred_path = os.path.join(os.path.dirname(__file__), 'serviceAccountKey.json')
 if not firebase_admin._apps:
-    cred = credentials.Certificate(cred_path)
+    firebase_key_str = os.environ.get("FIREBASE_KEY")
+    firebase_key_dict = json.loads(firebase_key_str.replace("\\n", "\n"))
+    cred = credentials.Certificate(firebase_key_dict)
+# Chỉ khởi tạo Firebase một lần
+# if not firebase_admin._apps:
+#     cred = credentials.Certificate(cred_path)
     firebase_admin.initialize_app(cred)
 
 def send_push_notification(device_token, title, body, data=None):
